cc20f3bef6c53dde71fabeac53e1b1ae1dfa8ae3
PC-Trabalhos <henrique.rosa07@outlook.com.br>
2019-04-23 17:22:01 -0300

    XML com jQuery/AJAX

--- unidade_04/exercicio.php
+++ unidade_04/exercicio.php
@@ -3,14 +3,42 @@
     <head>
         <meta charset="UTF-8">
         <title>PHP com AJAX</title>
+        <link href="_css/estilo.css" rel="stylesheet"></link>
     </head>
 
     <body>
+        <button id="botao">Carregar</button>
         <div id="listagem"></div>
         
         <script src="jquery.js"></script>
         <script>
+            $('button#botao').click(function(){
+                $('div#listagem').css('display','block');
+                carregarDados()
+            })
+            function carregarDados(){
+                $.ajax({
+                    url:'_xml/produtos.xml'
+                }).then(sucesso, falha);
 
+                function sucesso(dados){
+                    var elemento;
+                    elemento = "<ul>";
+                    $(dados).find('produto').each(function(){
+                        var nome = $(this).find('nomeproduto').text();
+                        var preco = $(this).find('precounitario').text();
+                        elemento += "<li class='nome'>" + nome  + "</li>";
+                        elemento += "<li class='preco'>" + preco  + "</li>"
+                    })
+                    elemento += "</ul>"
+
+                    $('div#listagem').html(elemento);
+                }
+
+                function falha(){
+
+                }
+            }
         </script>
     </body>
 </html>
