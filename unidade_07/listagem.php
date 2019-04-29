a54846fd4b2a854bfbdcf68f007f7760da609b74
PC-Trabalhos <henrique.rosa07@outlook.com.br>
2019-04-29 00:37:08 -0300

    CallBack

--- unidade_07/listagem.php
+++ unidade_07/listagem.php
@@ -3,10 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <title>PHP com AJAX</title>
-     
+        <script>
+            function retornaProdutos(dados){
+                console.log(dados);
+            }
+        </script>
     </head>
 
     <body>
-
+    <script src="http://localhost/PHP_AJAX/unidade_07/gerar_json.php?callback=retornaProdutos"></script>
     </body>
 </html>
