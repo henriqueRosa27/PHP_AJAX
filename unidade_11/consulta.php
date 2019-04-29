84ffcec9283579b78af4af7b61c6f245d34befc5
PC-Trabalhos <henrique.rosa07@outlook.com.br>
2019-04-29 16:58:31 -0300

    Lista aninhada

--- unidade_11/consulta.php
+++ unidade_11/consulta.php
@@ -13,6 +13,9 @@
                     <label for="categorias">Categorias</label>
                     <select id="categorias">
                     </select>
+                    <label for="produto">Categorias</label>
+                    <select id="produto">
+                    </select>
                 </form>
             </div>
         </main>
@@ -20,6 +23,34 @@
         
         <script src="_js/jquery.js"></script>
         <script>
+            function retornarCategorias(dados){
+                var categoria = "";
+                $.each(dados, function(chave, valor){
+                    categoria += "<option value=" + valor.categoriaID + ">" + valor.nomecategoria + "</option>";
+                })
+                $('#categorias').append(categoria);
+            }
+
+            $('#categorias').change(function(e){
+                var id = $(this).val();
+
+                $.ajax({
+                    type: "GET",
+                    data: "categoriaID=" + id,
+                    url: "retornar_produtos.php",
+                    async: false
+                }).done(function(data){
+                    //console.log($.parseJSON(data));
+                    var lista = "";
+                    $.each($.parseJSON(data), function(chave, valor){
+                        lista += "<option value=" + valor.produtoID + ">" + valor.nomeproduto + "</option>";
+                    });
+                    $('#produto').html(lista);
+                }).fail(function(){
+
+                });
+            });
         </script>
+        <script src="http://localhost/PHP_AJAX/unidade_11/retornar_categorias.php?callback=retornarCategorias"></script>
     </body>
 </html>
