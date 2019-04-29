18692fc2666428527bd1dc3386b5f46f4e562132
PC-Trabalhos <henrique.rosa07@outlook.com.br>
2019-04-28 21:40:56 -0300

    json

--- unidade_05/exercicio.php
+++ unidade_05/exercicio.php
@@ -3,13 +3,31 @@
     <head>
         <meta charset="UTF-8">
         <title>PHP com AJAX</title>
+        <link href="_css/estilo.css" rel="stylesheet">
     </head>
 
     <body>
+        <button id="botao">Carregar Dados</button>
         <div id="listagem"></div>
         <script src="jquery.js"></script>
         <script>
+            $('button#botao').click(function(){
+                $('div#listagem').css('display','block');
+                carregarDados();
+            })
+            function carregarDados(){
+                $.getJSON('_json/produtos.json', function(dado){
+                    var elemento;
+                    elemento = "<ul>";
+                    $.each(dado,function(i, valor){
+                        elemento += "<li class='nome'>" + valor.nomeproduto + "</li>";
+                        elemento += "<li class='preco'>" + valor.precounitario + "</li>";
+                    })
+                    elemento += "</ul>";
 
+                    $('div#listagem').html(elemento);
+                });
+            }
         </script>
     </body>
 </html>
